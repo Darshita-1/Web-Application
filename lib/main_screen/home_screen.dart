7a0a8fd4451da9705974e9dbccca02abc6b983a7
Darshita-1 <darshitasingh2001@gmail.com>
2023-04-17 15:14:23 +0530

    Initial commit

---
+++ lib/main_screen/home_screen.dart
@@ -0,0 +1,142 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String timeText = "";
+  String dateText = "";
+
+  String formatCurrentLiveTime(DateTime time) {
+    return DateFormat("hh:mm:ss a").format(time);
+  }
+
+  String formatCurrentDate(DateTime date) {
+    return DateFormat("dd MMMM,yyyy").format(date);
+  }
+
+  getcurrentLiveTime() {
+    final DateTime timeNow = DateTime.now();
+    final String liveTime = formatCurrentLiveTime(timeNow);
+    final String liveDate = formatCurrentDate(timeNow);
+
+    if (mounted) {
+      setState(() {
+        timeText = liveTime;
+        dateText = liveDate;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      getcurrentLiveTime();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text(
+          "Food delivery",
+          style:
+              TextStyle(fontWeight: FontWeight.bold, fontFamily: 'RobotoMono'),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+        toolbarHeight: 25,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  timeText + "\n" + dateText,
+                  style: const TextStyle(
+                    fontFamily: AutofillHints.streetAddressLevel1,
+                    fontSize: 15,
+                    //color: Colors.white,
+                    letterSpacing: 2,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'Images/icon-food.png',
+                width: MediaQuery.of(context).size.width * 0.15,
+                height: MediaQuery.of(context).size.width * 0.15,
+                fit: BoxFit.cover,
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton.icon(
+                  icon: const Icon(
+                    Icons.lock,
+                    color: Colors.black,
+                  ),
+                  label: const Text(
+                    "LOGIN",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      padding: const EdgeInsets.fromLTRB(70, 20, 70, 20)),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton.icon(
+                icon: const Icon(
+                  Icons.person,
+                  color: Colors.black,
+                ),
+                label: const Text(
+                  "REGISTER",
+                  style: TextStyle(color: Colors.black),
+                ),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    padding: const EdgeInsets.fromLTRB(60, 20, 60, 20)),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/* dateText = formatCurrentDate(DateTime.now());
+    //date
+    timeText = formatCurrentLiveTime(DateTime.now());
+    //time*/
